8c203d09b0a15e01d6b468bc8f6cc20dcd19455d
Ghulam Yahya Rajabi <rajabighulamyahya@gmail.com>
2024-01-30 13:08:05 -0700

    add

--- Labs/Lab 1/Lab 1.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-x = [1 , 2, 3]
-y = np.array([1, 2, 3])
-print('this is 3y', 3*y)
--- Labs/Lab 1/test2.py
+++ Labs/Lab 1/test2.py
@@ -1,1 +1,21 @@
-print('this is 3y')
+import numpy as np
+x = [1 , 2, 3]
+y = np.array([1, 2, 3])
+print('this is 3y', 3*y)
+import matplotlib.pyplot as plt
+X = np.linspace(0, 2* np.pi, 100)
+Ya = np.sin(X)
+Yb = np.cos(X)
+
+plt.plot(X, Ya)
+plt.plot(X, Yb)
+plt.show()
+
+X = np.linspace(0, 2*np.pi, 100)
+Ya = np.sin(X)
+Yb = np.cos(X)
+plt.plot(X, Ya)
+plt.plot(X, Yb)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
